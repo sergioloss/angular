570df03d6e9e86de898ea6d0350640fb04ef6974
sergioloss <sergio.sloss@gmail.com>
2016-06-21 23:41:17 -0300

    ScrollIf

--- Integra-0.2/scripts/angular-auto-scroll.js
+++ Integra-0.2/scripts/angular-auto-scroll.js
@@ -12,20 +12,50 @@ angular.module('auto-scroll', []).directive('autoScroll', function($timeout) {
       $scope.$parent.$watch(watch, function() {
         $timeout(function () {
           var elem = angular.element(el)[0];
-          var parent = angular.element(el).parent();
-            console.log('elem: ' + elem.tagName);
-            console.log('parent: ' + parent.tagName);
+          var child = el.children(':first')[0];
           //var $scrollTo = $(el).find(scrollTo);
           var $scrollTo = elem.getElementsByClassName(scrollTo);
 
-          console.log('$scrollTo.length = ' + $scrollTo.length);
           if($scrollTo.length){
             //$(el).scrollTop(0).scrollTop($scrollTo.offset().top - $(el).offset().top - offsetTop);  
-              console.log('el.parent() = ' + el.parent().tagName)
-            el.parent().scrollTo = 7; //($scrollTo.offset().top - elem.offset().top - offsetTop);  
+              console.log('$scrollTo[0].scrollTop = ' + $scrollTo[0].getBoundingClientRect().top);
+            elem.scrollTop = $scrollTo.scrollTop; //700; //($scrollTo.offset().top - elem.offset().top - offsetTop);  
           }
         });
       })
     }
   }
+});
+
+app.directive('scrollIf', function () {
+    
+    var getScrollingParent = function(element) {
+        element = element.parentElement;
+        while (element) {
+            if (element.scrollHeight !== element.clientHeight) {
+                return element;
+            }
+            element = element.parentElement;
+        }
+        return null;
+    };
+    
+    return function (scope, element, attrs) {
+        scope.$watch(attrs.scrollIf, function(value) {
+            if (value) {
+                var sp = getScrollingParent(element[0]);
+                var topMargin = parseInt(attrs.scrollMarginTop) || 0;
+                var bottomMargin = parseInt(attrs.scrollMarginBottom) || 0;
+                var elemOffset = element[0].offsetTop;
+                var elemHeight = element[0].clientHeight;
+
+                if (elemOffset - topMargin < sp.scrollTop) {
+                    sp.scrollTop = elemOffset - topMargin;
+                } else if (elemOffset + elemHeight + bottomMargin > sp.scrollTop + sp.clientHeight) {
+                    sp.scrollTop = elemOffset + elemHeight + bottomMargin - sp.clientHeight;
+                }
+            }
+        });
+    }
+    
 });
